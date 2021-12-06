cb43984f34574089e5f1007a829e98e725c671be
Mame_ym86@itstep.edu.az <Mame_ym86@itstep.edu.az>
2021-12-06 21:49:47 +0400

    .

--- SystemProgramming_homework1/Form1.Designer.cs
+++ SystemProgramming_homework1/Form1.Designer.cs
@@ -28,13 +28,194 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lowerBoundTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.upperBoundTextBox = new System.Windows.Forms.TextBox();
+            this.startPrimeNumbersButton = new System.Windows.Forms.Button();
+            this.primeNumbersTextBox = new System.Windows.Forms.TextBox();
+            this.fibonacciNumbersTextBox = new System.Windows.Forms.TextBox();
+            this.startFibonacciButton = new System.Windows.Forms.Button();
+            this.stopPrimeButton = new System.Windows.Forms.Button();
+            this.stopFibonacciButton = new System.Windows.Forms.Button();
+            this.pausePrimeNumbersButton = new System.Windows.Forms.Button();
+            this.resumePrimeNumbersButton = new System.Windows.Forms.Button();
+            this.restartPrimeButton = new System.Windows.Forms.Button();
+            this.restartFibonacciButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lower Bound: ";
+            // 
+            // lowerBoundTextBox
+            // 
+            this.lowerBoundTextBox.Location = new System.Drawing.Point(143, 6);
+            this.lowerBoundTextBox.Name = "lowerBoundTextBox";
+            this.lowerBoundTextBox.Size = new System.Drawing.Size(125, 31);
+            this.lowerBoundTextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(299, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Upper Bound: ";
+            // 
+            // upperBoundTextBox
+            // 
+            this.upperBoundTextBox.Location = new System.Drawing.Point(432, 9);
+            this.upperBoundTextBox.Name = "upperBoundTextBox";
+            this.upperBoundTextBox.Size = new System.Drawing.Size(125, 31);
+            this.upperBoundTextBox.TabIndex = 3;
+            // 
+            // startPrimeNumbersButton
+            // 
+            this.startPrimeNumbersButton.Location = new System.Drawing.Point(597, 9);
+            this.startPrimeNumbersButton.Name = "startPrimeNumbersButton";
+            this.startPrimeNumbersButton.Size = new System.Drawing.Size(132, 31);
+            this.startPrimeNumbersButton.TabIndex = 4;
+            this.startPrimeNumbersButton.Text = "Start(Prime)";
+            this.startPrimeNumbersButton.UseVisualStyleBackColor = true;
+            this.startPrimeNumbersButton.Click += new System.EventHandler(this.startPrimeNumbersButton_Click);
+            // 
+            // primeNumbersTextBox
+            // 
+            this.primeNumbersTextBox.Location = new System.Drawing.Point(12, 55);
+            this.primeNumbersTextBox.Multiline = true;
+            this.primeNumbersTextBox.Name = "primeNumbersTextBox";
+            this.primeNumbersTextBox.Size = new System.Drawing.Size(717, 88);
+            this.primeNumbersTextBox.TabIndex = 5;
+            // 
+            // fibonacciNumbersTextBox
+            // 
+            this.fibonacciNumbersTextBox.Location = new System.Drawing.Point(12, 285);
+            this.fibonacciNumbersTextBox.Multiline = true;
+            this.fibonacciNumbersTextBox.Name = "fibonacciNumbersTextBox";
+            this.fibonacciNumbersTextBox.Size = new System.Drawing.Size(717, 84);
+            this.fibonacciNumbersTextBox.TabIndex = 6;
+            // 
+            // startFibonacciButton
+            // 
+            this.startFibonacciButton.Location = new System.Drawing.Point(12, 235);
+            this.startFibonacciButton.Name = "startFibonacciButton";
+            this.startFibonacciButton.Size = new System.Drawing.Size(153, 34);
+            this.startFibonacciButton.TabIndex = 7;
+            this.startFibonacciButton.Text = "Start(fibonacci)";
+            this.startFibonacciButton.UseVisualStyleBackColor = true;
+            this.startFibonacciButton.Click += new System.EventHandler(this.startFibonacciButton_Click);
+            // 
+            // stopPrimeButton
+            // 
+            this.stopPrimeButton.Location = new System.Drawing.Point(12, 149);
+            this.stopPrimeButton.Name = "stopPrimeButton";
+            this.stopPrimeButton.Size = new System.Drawing.Size(112, 34);
+            this.stopPrimeButton.TabIndex = 8;
+            this.stopPrimeButton.Text = "Stop(Prime)";
+            this.stopPrimeButton.UseVisualStyleBackColor = true;
+            this.stopPrimeButton.Click += new System.EventHandler(this.stopPrimeButton_Click);
+            // 
+            // stopFibonacciButton
+            // 
+            this.stopFibonacciButton.Location = new System.Drawing.Point(12, 375);
+            this.stopFibonacciButton.Name = "stopFibonacciButton";
+            this.stopFibonacciButton.Size = new System.Drawing.Size(153, 34);
+            this.stopFibonacciButton.TabIndex = 9;
+            this.stopFibonacciButton.Text = "Stop(fibonacci)";
+            this.stopFibonacciButton.UseVisualStyleBackColor = true;
+            this.stopFibonacciButton.Click += new System.EventHandler(this.stopFibonacciButton_Click);
+            // 
+            // pausePrimeNumbersButton
+            // 
+            this.pausePrimeNumbersButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.pausePrimeNumbersButton.Location = new System.Drawing.Point(156, 149);
+            this.pausePrimeNumbersButton.Name = "pausePrimeNumbersButton";
+            this.pausePrimeNumbersButton.Size = new System.Drawing.Size(112, 34);
+            this.pausePrimeNumbersButton.TabIndex = 10;
+            this.pausePrimeNumbersButton.Text = "Pause";
+            this.pausePrimeNumbersButton.UseVisualStyleBackColor = true;
+            this.pausePrimeNumbersButton.Click += new System.EventHandler(this.pausePrimeNumbersButton_Click);
+            // 
+            // resumePrimeNumbersButton
+            // 
+            this.resumePrimeNumbersButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.resumePrimeNumbersButton.Location = new System.Drawing.Point(299, 149);
+            this.resumePrimeNumbersButton.Name = "resumePrimeNumbersButton";
+            this.resumePrimeNumbersButton.Size = new System.Drawing.Size(112, 34);
+            this.resumePrimeNumbersButton.TabIndex = 11;
+            this.resumePrimeNumbersButton.Text = "Resume";
+            this.resumePrimeNumbersButton.UseVisualStyleBackColor = true;
+            this.resumePrimeNumbersButton.Click += new System.EventHandler(this.resumePrimeNumbersButton_Click);
+            // 
+            // restartPrimeButton
+            // 
+            this.restartPrimeButton.Location = new System.Drawing.Point(432, 149);
+            this.restartPrimeButton.Name = "restartPrimeButton";
+            this.restartPrimeButton.Size = new System.Drawing.Size(112, 34);
+            this.restartPrimeButton.TabIndex = 12;
+            this.restartPrimeButton.Text = "Restart";
+            this.restartPrimeButton.UseVisualStyleBackColor = true;
+            this.restartPrimeButton.Click += new System.EventHandler(this.restartPrimeButton_Click);
+            // 
+            // restartFibonacciButton
+            // 
+            this.restartFibonacciButton.Location = new System.Drawing.Point(186, 375);
+            this.restartFibonacciButton.Name = "restartFibonacciButton";
+            this.restartFibonacciButton.Size = new System.Drawing.Size(112, 34);
+            this.restartFibonacciButton.TabIndex = 13;
+            this.restartFibonacciButton.Text = "Restart";
+            this.restartFibonacciButton.UseVisualStyleBackColor = true;
+            this.restartFibonacciButton.Click += new System.EventHandler(this.restartFibonacciButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(744, 450);
+            this.Controls.Add(this.restartFibonacciButton);
+            this.Controls.Add(this.restartPrimeButton);
+            this.Controls.Add(this.resumePrimeNumbersButton);
+            this.Controls.Add(this.pausePrimeNumbersButton);
+            this.Controls.Add(this.stopFibonacciButton);
+            this.Controls.Add(this.stopPrimeButton);
+            this.Controls.Add(this.startFibonacciButton);
+            this.Controls.Add(this.fibonacciNumbersTextBox);
+            this.Controls.Add(this.primeNumbersTextBox);
+            this.Controls.Add(this.startPrimeNumbersButton);
+            this.Controls.Add(this.upperBoundTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lowerBoundTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox lowerBoundTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox upperBoundTextBox;
+        private System.Windows.Forms.Button startPrimeNumbersButton;
+        private System.Windows.Forms.TextBox primeNumbersTextBox;
+        private System.Windows.Forms.TextBox fibonacciNumbersTextBox;
+        private System.Windows.Forms.Button startFibonacciButton;
+        private System.Windows.Forms.Button stopPrimeButton;
+        private System.Windows.Forms.Button stopFibonacciButton;
+        private System.Windows.Forms.Button pausePrimeNumbersButton;
+        private System.Windows.Forms.Button resumePrimeNumbersButton;
+        private System.Windows.Forms.Button restartPrimeButton;
+        private System.Windows.Forms.Button restartFibonacciButton;
     }
 }
 
--- SystemProgramming_homework1/Form1.cs
+++ SystemProgramming_homework1/Form1.cs
@@ -1,21 +1,188 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace SystemProgramming_homework1
 {
     public partial class Form1 : Form
     {
+        private bool primeHasStarted = false;
+        private bool fibonacciHasStarted = false;
+
+        private bool stopPrime = false;
+        private bool stopFibonacci = false;
+
+        private bool continuePrime = true;
+        private bool continueFibonacci = true;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+
+
+        private void startPrimeNumbersButton_Click(object sender, EventArgs e)
+        {
+            if (!primeHasStarted)
+                ThreadPool.QueueUserWorkItem(PrintPrimeNumbers);
+
+            primeHasStarted = true;
+        }
+
+        private void stopPrimeButton_Click(object sender, EventArgs e)
+        {
+            stopPrime = true;
+        }
+
+        private void pausePrimeNumbersButton_Click(object sender, EventArgs e)
+        {
+            continuePrime = false;
+        }
+
+        private void resumePrimeNumbersButton_Click(object sender, EventArgs e)
+        {
+            continuePrime = true;
+        }
+
+        private void restartPrimeButton_Click(object sender, EventArgs e)
+        {
+            primeNumbersTextBox.Text = String.Empty;
+            ThreadPool.QueueUserWorkItem(PrintPrimeNumbers);
+            stopPrime = false;
+        }
+
+
+
+
+        private void startFibonacciButton_Click(object sender, EventArgs e)
+        {
+            if (!fibonacciHasStarted)
+                ThreadPool.QueueUserWorkItem(PrintFibonacci);
+
+            fibonacciHasStarted = true;
+        }
+
+        private void stopFibonacciButton_Click(object sender, EventArgs e)
+        {
+            stopFibonacci = true;
+        }
+
+        private void restartFibonacciButton_Click(object sender, EventArgs e)
+        {
+            fibonacciNumbersTextBox.Text = String.Empty;
+            ThreadPool.QueueUserWorkItem(PrintFibonacci);
+            stopFibonacci = false;
+        }
+
+
+
+
+        private void PrintFibonacci(object obj)
+        {
+            if (!stopFibonacci)
+            {
+                fibonacciNumbersTextBox.Text += "0 ";
+                fibonacciNumbersTextBox.Text += "1 ";
+            }
+
+            long prevprev = 0;
+            long prev = 1;
+            while (!stopFibonacci)
+            {
+                long next = GetNextFibonacci(prevprev, prev);
+                fibonacciNumbersTextBox.Text += $"{ next } ";
+
+                prevprev = prev;
+                prev = next;
+                Thread.Sleep(750);
+            }
+        }
+
+
+        private void PrintPrimeNumbers(object obj)
+        {
+            long lb = 2;
+            long ub = -1;
+
+            if (lowerBoundTextBox.Text != String.Empty)
+            {
+                try
+                {
+                    lb = long.Parse(lowerBoundTextBox.Text);
+                }
+                catch (Exception)
+                {
+                    lb = 2;
+                }
+            }
+
+            if (upperBoundTextBox.Text != String.Empty)
+
+            {
+                try
+                {
+                    ub = long.Parse(upperBoundTextBox.Text);
+                }
+                catch (Exception)
+                {
+                    ub = -1;
+                }
+            }
+
+            if (!stopPrime)
+            {
+                if (ub != -1)  // there is upper bound
+                {
+                    for (long i = lb; i <= ub; i++)
+                    {
+                        if (stopPrime) break;
+
+                        if (IsPrime(i))
+                        {
+                            primeNumbersTextBox.Text += $"{i} ";
+                            Thread.Sleep(650);
+                        }
+                    }
+                }
+
+                else  // no upper bound
+                {
+                    for (long i = lb; ; i++)
+                    {
+                        if (stopPrime) break;
+
+                        if (IsPrime(i))
+                        {
+                            primeNumbersTextBox.Text += $"{i} ";
+                            Thread.Sleep(650);
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private bool IsPrime(long n)
+        {
+            if (n <= 1) return false;
+            if (n == 2) return true;
+            if (n % 2 == 0) return false;
+
+            for (int i = 3; i <= (int)Math.Floor(Math.Sqrt(n)); i += 2)
+                if (n % i == 0)
+                    return false;
+
+            return true;
+        }
+
+
+        private long GetNextFibonacci(long prevprev = 0, long prev = 1)
+        {
+            return prevprev + prev;
+        }
+
+        
+ 
     }
 }
